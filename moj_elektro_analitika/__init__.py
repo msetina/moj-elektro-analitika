7ff2e9cbf97e8e6aad4d9728ec566a8829865027
Miha Šetina <miha.setina@gmail.com>
2024-07-03 17:19:03 +0200

    making lint happy

--- moj_elektro_analitika/__init__.py
+++ moj_elektro_analitika/__init__.py
@@ -1,3 +1,6 @@
+from moj_elektro_analitika.buckets_poraba_moj_elektro import (
+    BucketsPorabaMojElektro as BucketsPorabaMojElektro,
+)
 from moj_elektro_analitika.dogovorjena_moc_moj_elektro import (
     DogovorjenaMocMojElektro as DogovorjenaMocMojElektro,
 )
@@ -9,7 +12,4 @@ from moj_elektro_analitika.presezki_moj_elektro import (
 )
 from moj_elektro_analitika.racun_moj_elektro import (
     RacunMojElektro as RacunMojElektro,
-)
-from moj_elektro_analitika.buckets_poraba_moj_elektro import (
-    BucketsPorabaMojElektro as BucketsPorabaMojElektro,
 )
